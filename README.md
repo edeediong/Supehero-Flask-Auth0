7c038b36257e94c954efc232ab3ddfe5fa68323d
Edidiong Etuk <edidiongmichael08@gmail.com>
2021-10-19 00:19:38 +0100

    updated repository with latest changes
    Signed-off-by: Edidiong Etuk <edidiongmichael08@gmail.com>

--- README.md
+++ README.md
@@ -3,46 +3,30 @@ This repository contains the code of a Supehero based website with authenticatio
 
 ## Prerequisites
 - virtualenv
-- Superhero [API Access Key](https://superheroapi.com/)
 
 ### Install VirtualEnv
 ```bash
   sudo pip install virtualenv
 ```
 
-### Get Superhero API Access Key
-Head over to the website given above  and generate  your access token.
-
-## Initialize Code
-
-### Clone Branch
-
-We clone the branch **initial** in the repo.
-
-```bash
-  git clone -b initial https://github.com/edeediong/Supehero-Flask-Auth0.git
-```
-
-Here's the structure of our project after running `tree Supehero-Flask-Auth0`.
+Here's the structure of our project
 
 ```bash
 .
 ├── app.py
 ├── requirements.txt
 ├── static
-│   ├── css
-│   │   └── main.css
-│   └── images
-│       ├── 1158056.jpg
-│       └── 703e02b53cb97fa45cef8b156e4b0e4a.jpg
+│   ├── css
+│   │   └── main.css
+│   └── images
+│       ├── 1158056.jpg
+│       └── 703e02b53cb97fa45cef8b156e4b0e4a.jpg
 ├── templates
-    ├── homepage.html
-    └── results.html
+    ├── homepage.html
+    └── results.html
 ```
 
-### Activate Python Environment with Necessary Libraries
-
-Then activate virtual env and install requirements.txt
+We clone the repo, then activate virtual env and install requirements.txt
 
 ```bash
   virtualenv my_env
@@ -50,8 +34,6 @@ Then activate virtual env and install requirements.txt
 
   pip install -r requirements.txt
 ```
-
-### Attach Environment Variables
 
 Next step is writing our environment variables in our `.env` file.
 
@@ -66,7 +48,7 @@ We are storing keys gotten from our SuperHero API in our `.env` file.
 
 ## Starting the App
 
-While the virtual evironment is activated, we enter this on local terminal
+While the virtual evironment is activated, we enter this on our terminal
 
 ```bash
   flask run
--- app.py
+++ app.py
@@ -1,97 +1,38 @@
 import os
-import requests
-import json
-
 from flask import Flask, render_template, request, session, jsonify, redirect, url_for
 from dotenv import load_dotenv
-from authlib.integrations.flask_client import OAuth
-from six.moves.urllib.parse import urlencode
+import requests
+import json
 
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY")
+app.secret_key = os.environ.get('SECRET_KEY')
 
-oauth = OAuth(app)
-
-auth0 = oauth.register(
-    "auth0",
-    client_id=os.environ.get("CLIENT_ID"),
-    client_secret=os.environ.get("CLIENT_SECRET"),
-    api_base_url=os.environ.get("API_BASE_URL"),
-    access_token_url=os.environ.get("API_BASE_URL") + "/oauth/token",
-    authorize_url=os.environ.get("API_BASE_URL") + "/authorize",
-    client_kwargs={"scope": "openid profile"},
-)
-
-# Client Kwargs are specified in order to return user info when successfully logged in
-
-
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "GET":
-        return render_template("homepage.html", user=session.get("jwt_payload"))
+        return render_template('homepage.html')
     elif request.method == "POST":
-        name = request.form.get("supe-name")
-        url = (
-            "https://superheroapi.com/api/"
-            + os.environ.get("API_ACCESS_KEY")
-            + "/search/"
-            + name
-        )
+        name = request.form.get('supe-name')
+        url = 'https://akabab.github.io/superhero-api/api/all.json'
 
         results = requests.get(url).json()
 
-        session["results"] = results
-        return redirect(url_for("results"))
+        supe_details = []
+        for x in results:
+            if (name.lower() in x['name'].lower()):
+                supe_details.append(x)
 
+        session['results'] = supe_details
+        
+        return redirect(url_for('results'))
 
-@app.route("/callback")
-def handle_callback():
-    """This verifies the user's identify when logged in and returns the user data in json."""
-    auth0.authorize_access_token()
-    resp = auth0.get("userinfo")
-    user_info = resp.json()
-
-    # Save User Data to our session
-    session["jwt_payload"] = user_info
-
-    # If the user searched for something before logging in, redirect to results
-    if session.get("results") is not None:
-        return redirect(url_for("results"))
-
-    return redirect(url_for("index"))
-
-
-@app.get("/results")
+@app.route('/results', methods=['GET'])
 def results():
-    results = session.get("results")
-    user_info = session.get("jwt_payload")
-
-    if user_info is not None:
-        if results is not None:
-            return render_template("results.html", data=results, user=user_info)
-
-    return redirect("/login")
+    results = session.get('results')
+    return render_template('results.html', data=results)
 
 
-@app.get("/login")
-def login():
-    # Auth0 authorizes our callback route and redirects to it's login page
-    return auth0.authorize_redirect(redirect_uri=os.get.environ("YOUR_DOMAIN") + "/callback")
-
-
-@app.get("/logout")
-def logout():
-    session.clear()
-    # Redirect user to logout endpoint
-    params = {
-        "returnTo": url_for("index", _external=True),
-        "client_id": os.environ.get("CLIENT_ID"),
-    }
-    return redirect(auth0.api_base_url + "/v2/logout?" + urlencode(params))
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
